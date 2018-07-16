5a547361171f6f50a3e53750167e4c668c0cae2e
szakon <sarahzakon@gmail.com>
2018-07-16 13:53:08 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,45 @@
 # Code your cash register here!
+
+class AmazonTransaction
+  
+  attr_accessor :total, :items, :discount
+
+  def initialize(discount = 20)
+    @total = 0
+    @items = []
+    @discount = discount
+    
+  end
+  
+
+  
+  def add_item(title, price, quantity = 1)
+    
+    @total += price*quantity
+    items << title
+  
+  end
+  
+  def apply_discount
+    @total -= @total*0.2 
+    
+  end
+  
+  
+end
+
+
+
+
+
+
+#def amazon_prime
+  #input =gets.chomp
+  #if input = "amazon_prime"
+ #   shipping = 0
+#  else 
+  #  shipping = shipping
+ # end
+    
+    
+#end
